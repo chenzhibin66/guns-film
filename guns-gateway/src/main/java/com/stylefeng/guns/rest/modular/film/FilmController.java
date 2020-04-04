ea55c8cee3920752c1c746f4c4d2badb9029d009
chenzhibin <calvinchen66@163.com>
2020-04-04 22:12:30 +0800

    条件查询

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -1,6 +1,10 @@
 package com.stylefeng.guns.rest.modular.film;
 
 import com.stylefeng.guns.api.film.FilmServiceAPI;
+import com.stylefeng.guns.api.film.vo.CatVO;
+import com.stylefeng.guns.api.film.vo.SourceVO;
+import com.stylefeng.guns.api.film.vo.YearVO;
+import com.stylefeng.guns.rest.modular.film.vo.FilmConditionVO;
 import com.stylefeng.guns.rest.modular.film.vo.FilmIndexVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.apache.dubbo.config.annotation.Reference;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author chenzhibin
@@ -45,7 +52,90 @@ public class FilmController {
     public ResponseVO getConditionList(@RequestParam(value = "catId", required = false, defaultValue = "99") String catId,
                                        @RequestParam(value = "sourceId", required = false, defaultValue = "99") String sourceId,
                                        @RequestParam(value = "yearId", required = false, defaultValue = "99") String yearId) {
+        FilmConditionVO filmConditionVO = new FilmConditionVO();
+        boolean flag = false;
         //类型集合
-        return null;
+        FilmConditionVO conditionVO = new FilmConditionVO();
+        List<CatVO> cats = filmServiceAPI.getCats();
+        List<CatVO> catResult = new ArrayList<>();
+        CatVO cat = null;
+        for (CatVO catVO : cats) {
+            //判断集合是否存在catId,如果存在将对应的实体变成active状态
+            if (catVO.getCatId().equals("99")) {
+                cat = catVO;
+                continue;
+            }
+            if (catVO.getCatId().equals(catId)) {
+                flag = true;
+                catVO.setActive(true);
+            } else {
+                catVO.setActive(false);
+            }
+            catResult.add(catVO);
+            //如果不存在,则默认将全部变为Active状态
+            if (!flag) {
+                cat.setActive(true);
+                catResult.add(cat);
+            } else {
+                cat.setActive(false);
+                catResult.add(cat);
+            }
+
+        }
+        //片源集合
+        flag = false;
+        List<SourceVO> sources = filmServiceAPI.getSources();
+        List<SourceVO> sourceResult = new ArrayList<>();
+        SourceVO sourceVO = null;
+        for (SourceVO source : sources) {
+            if (source.getSourceId().equals("99")) {
+                sourceVO = source;
+                continue;
+            }
+            if (source.getSourceId().equals(sourceId)) {
+                flag = true;
+                source.setActive(true);
+            } else {
+                source.setActive(false);
+            }
+            sourceResult.add(source);
+            if (!flag) {
+                sourceVO.setActive(true);
+                sourceResult.add(sourceVO);
+            } else {
+                sourceVO.setActive(false);
+                sourceResult.add(sourceVO);
+            }
+        }
+        //年代集合
+        flag = false;
+        List<YearVO> years = filmServiceAPI.getYears();
+        List<YearVO> yearResult = new ArrayList<>();
+        YearVO yearVO = null;
+        for (YearVO year : years) {
+            if (year.getYearId().equals("99")) {
+                yearVO = year;
+                continue;
+            }
+            if (year.getYearId().equals(yearId)) {
+                flag = true;
+                year.setActive(true);
+            } else {
+                year.setActive(false);
+            }
+            yearResult.add(year);
+        }
+        //如果不存在,则默认将全部变为active状态
+        if (!flag) {
+            yearVO.setActive(true);
+            yearResult.add(yearVO);
+        } else {
+            yearVO.setActive(false);
+            yearResult.add(yearVO);
+        }
+        filmConditionVO.setCatInfo(catResult);
+        filmConditionVO.setSourceInfo(sourceResult);
+        filmConditionVO.setYearInfo(yearResult);
+        return ResponseVO.success(filmConditionVO);
     }
 }
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/vo/FilmConditionVO.java
@@ -0,0 +1,19 @@
+package com.stylefeng.guns.rest.modular.film.vo;
+
+import com.stylefeng.guns.api.film.vo.CatVO;
+import com.stylefeng.guns.api.film.vo.SourceVO;
+import com.stylefeng.guns.api.film.vo.YearVO;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author chenzhibin
+ * @time 2020/4/4 17:47
+ */
+@Data
+public class FilmConditionVO {
+    private List<CatVO> catInfo;
+    private List<SourceVO> sourceInfo;
+    private List<YearVO> yearInfo;
+}
