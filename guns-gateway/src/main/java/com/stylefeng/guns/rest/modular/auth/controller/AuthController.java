5a8e428778e5db90b3759153db7e9093a0105c7d
chenzhibin <calvinchen66@163.com>
2020-05-06 16:19:59 +0800

    update

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
@@ -29,7 +29,7 @@ public class AuthController {
     public ResponseVO createAuthenticationToken(AuthRequest authRequest) {
 
         boolean validate = true;
-        // 去掉guns自身携带的用户名密码验证机制，使用我们自己的
+        // 去掉guns自身携带的用户名密码验证机制，使用自己的
         int userId = userAPI.login(authRequest.getUserName(), authRequest.getPassword());
         if (userId == 0) {
             validate = false;
